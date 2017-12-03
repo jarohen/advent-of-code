eb80eff90d617c4a7a5d3304c9ba9b5c8f870a9e
James Henderson <james@jarohen.me.uk>
2017-12-03 22:15:19 +0000

    Special casing the odd sqrts

--- src/advent_of_code/day3.clj
+++ src/advent_of_code/day3.clj
@@ -5,11 +5,14 @@
 (defn pt1 [x]
   (let [sqrt (long (Math/sqrt (float x)))
         odd-sqrt (cond-> sqrt
-                   (even? sqrt) dec)
-        ring (/ (inc odd-sqrt) 2)
-        offset (- x (* odd-sqrt odd-sqrt))
-        segment (quot offset ring)
-        segment-offset (mod offset (* segment ring))]
-    (+ ring (if (even? segment)
-              (- ring segment-offset)
-              segment-offset))))
+                   (even? sqrt) dec)]
+    (if (= x (* odd-sqrt odd-sqrt))
+      (dec odd-sqrt)
+
+      (let [ring (/ (inc odd-sqrt) 2)
+            offset (- x (* odd-sqrt odd-sqrt))
+            segment (quot offset ring)
+            segment-offset (mod offset (* segment ring))]
+        (+ ring (if (even? segment)
+                  (- ring segment-offset)
+                  segment-offset))))))
