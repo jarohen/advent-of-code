6f84848a3da9160e94180eaf59ee21b3fe985b90
James Henderson <james@jarohen.me.uk>
2018-12-03 12:59:25 +0000

    didn't commit day2 yesterday :|

---
+++ 2018/src/aoc2018/day2.clj
@@ -0,0 +1,37 @@
+(ns aoc2018.day2
+  (:require [aoc2018.files :as files]
+            [clojure.test :as t]))
+
+(defn checksum [ids]
+  (let [{:keys [twos threes]} (->> ids
+                                   (reduce (fn [{:keys [twos threes]} id]
+                                             (let [freqs (set (vals (frequencies id)))]
+                                               {:twos (+ twos (if (contains? freqs 2) 1 0))
+                                                :threes (+ threes (if (contains? freqs 3) 1 0))}))
+                                           {:twos 0, :threes 0}))]
+    (* twos threes)))
+
+(t/deftest test-checksum
+  (t/is (= 12 (checksum ["abcdef" "bababc" "abbcde" "abcccd" "aabcdd" "abcdee" "ababab"]))))
+
+(comment
+  (files/with-line-seq "day2.txt" checksum))
+
+(defn different-characters [id1 id2]
+  (count (filter true? (map not= id1 id2))))
+
+(defn similar [ids]
+  (->> (for [id1 ids
+             id2 ids
+             :when (and (not= id1 id2)
+                        (= 1 (different-characters id1 id2)))]
+         #{id1 id2})
+
+       first))
+
+(t/deftest find-similar-pair
+  (t/is (= #{"fghij" "fguij"}
+           (similar ["abcde" "fghij" "klmno" "pqrst" "fguij" "axcye" "wvxyz"]))))
+
+(comment
+  (files/with-line-seq "day2.txt" similar))
